5b4fadfddc9fae1538cd9c1ce0ba425657e97649
Schuyler Audley-Williams <audley-williams.s@etoncollege.org.uk>
2022-02-10 13:16:37 +0000

    Added to UI

--- CommentCardWriter/ContentView.swift
+++ CommentCardWriter/ContentView.swift
@@ -11,23 +11,23 @@ struct ContentView: View {
     @StateObject private var commentCard = CommentCard()
     @State private var specificComments: String  = ""
     @State private var subject: String = ""
-    //@State private var comment: String = "In \(subject), \(commentCard.comment) \(specificComments)"
     
     var body: some View {
         
-        VStack {
-            Section {
-                Toggle("Are you happy?", isOn: $commentCard.isHappy)
-                Toggle("Is your attainment high?", isOn: $commentCard.hasHighAttainment)
-                Toggle("Do you need to improve?", isOn: $commentCard.needsImprovement)
-                TextField("Specific Comments", text: $specificComments).textFieldStyle(.roundedBorder)
-                TextField("Subject", text: $subject).textFieldStyle(.roundedBorder)
-            }
+        Section {
+            Toggle("Are you happy?", isOn: $commentCard.isHappy)
+            Toggle("Is your attainment high?", isOn: $commentCard.hasHighAttainment)
+            Toggle("Do you need to improve?", isOn: $commentCard.needsImprovement)
+            TextField("Specific Comments", text: $specificComments).textFieldStyle(.roundedBorder)
+            TextField("Subject", text: $subject).textFieldStyle(.roundedBorder)
+        }
+        VStack{
             Text("In \(subject), \(commentCard.comment) \(specificComments)").foregroundColor(Color.red)
+        }
             Spacer()
         }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
